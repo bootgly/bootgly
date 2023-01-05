0f6d10fc1bc66da8ea47b7fec4c159f6071954d1
Rodrigo Vieira <rodrigo@slayer.tech>
2023-01-05 19:34:15 -0300

    [Web\HTTP\Server\@\Connections\Data] - MINOR: Refactored: -- Moved HTTP Request/Response Data and Buffer set to Response -- Removed unnecessary read method override

--- nodes/Web/HTTP/Server/@/Connections/Data.php
+++ nodes/Web/HTTP/Server/@/Connections/Data.php
@@ -11,6 +11,7 @@
 namespace Bootgly\Web\HTTP\Server\_\Connections;
 
 
+use Bootgly\Web\TCP\Server;
 use Bootgly\Web\TCP\Server\Connection;
 use Bootgly\Web\TCP\Server\Connections\Data as TCPData;
 
@@ -30,89 +31,40 @@ class Data extends TCPData
       parent::__construct($Connection);
    }
 
-   public function read (&$Socket, bool $write = false) : bool
-   {
-      $read = parent::read($Socket, $write);
-
-      if ($read === false) {
-         return false;
-      }
-
-      // Do something...?
-
-      if ($write === false) {
-         #Server::$Event->add($Socket, Server::$Event::EVENT_WRITE, 'write');
-         $this->write($Socket);
-      }
-
-      return true;
-   }
    public function write (&$Socket, bool $handle = false, ? int $length = null) : bool
    {
       if (self::$input === '') {
          return false;
       }
 
-      // @ Check cache
-      // TODO Response cache (dynamic response)
-      $writed = false;
-      #if ($this->changed === false && $this->output !== '') {
-      #   parent::write($Socket, $handle);
-      #   @fflush($Socket);
-      #   return true;
-      #}
-
       // @ Instance callbacks
       $Request = $this->callbacks[0];
       $Response = $this->callbacks[1];
-      $Router = $this->callbacks[2];
+      // $Router = $this->callbacks[2];
 
-      // @ Set HTTP Data
-      // ! HTTP
-      // ? Response Content
-      $Response->Content->raw = ($this->Connection->handler)(
-         $Request,
-         $Response,
-         $Router
-      );
-      $Response->Content->length = strlen($Response->Content->raw);
-      // ? Response Header
-      // ...
-      $Response->Header->set('Content-Length', $Response->Content->length);
-      // ? Response Meta
-      // ...
-
-      // TODO implement Timer Event loop with tick to update this every 1 second
-      #$date = gmdate('D, d M Y H:i:s T');
-
-      // @ Set Buffer output
-      $this->output = <<<HTTP_RAW
-      {$Response->Meta->raw}
-      {$Response->Header->raw}
-      {$Response->Content->raw}
-      HTTP_RAW;
-
-      #$this->log($this->output . PHP_EOL);
+      // @ Set HTTP Request/Response Data and Output Buffer
+      $this->output = $Response->parse($this->Connection->handler, $this->callbacks);
 
       // @ Send data to client
-      $writed ?: $writed = parent::write($Socket, $handle, strlen($this->output));
+      $writed = parent::write($Socket, $handle, strlen($this->output));
       if ($writed === false) {
          return false;
       }
 
       // @ On success
-      // Delete event from loop
-      #Server::$Event->del($Socket, Server::$Event::EVENT_WRITE);
-      // Reset Request data
       if ($this->changed) {
+         // Delete event from loop
+         #Server::$Event->del($Socket, Server::$Event::EVENT_WRITE);
+
+         // Reset HTTP Request/Response Data
          $Request->reset();
-         $Response->Header->raw = '';
-      }
-      // Reset Buffer I/O
-      #self::$input = '';
-      #$this->output = '';
-      // Reset Parser
-      if ($this->changed) {
+         $Response->reset();
+
+         // Reset Buffer I/O
+         #self::$input = '';
+         #$this->output = '';
+
+         // Reset Parser
          self::$parsed = false;
          self::$parsing = false;
          self::$pointer = 0;
