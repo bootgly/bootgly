1188927ce6b457036e98f02b8c59799b1ab5f1f8
Rodrigo Vieira <rodrigo@slayer.tech>
2023-01-11 20:34:27 -0300

    Set CPU cores server usage to 55% while using JIT

--- server@http.php
+++ server@http.php
@@ -25,7 +25,7 @@ $HTTPServer = new HTTP\Server($Web);
 $HTTPServer->configure(
    host: '0.0.0.0',
    port: 8080,
-   workers: round( ((int) shell_exec('nproc')) * 0.6 ),
+   workers: round( ((int) shell_exec('nproc')) * 0.55 ), // Without JIT: * 0.6
 
    handler: function (Request $Request, Response $Response, Router $Router) {
       #$Request->method;    // GET
@@ -40,4 +40,11 @@ $HTTPServer->configure(
 $HTTPServer->start();
 
 // Benchmark test suggestion with 512 connections:
+
+// --- If your CPU have 24 CPU cores:
+
+// # Without JIT enabled: ~40% of CPU
 // wrk -t10 -c514 -d10s http://localhost:8080
+
+// # With JIT enabled: ~45% of CPU
+// wrk -t11 -c514 -d10s http://localhost:8080
