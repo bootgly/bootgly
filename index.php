61324acda39f089a7b2d4e56d5d2849e76432dde
Rodrigo Vieira <rodrigo@slayer.tech>
2023-01-12 18:03:47 -0300

    🔥 Remove duplicated const from previous loader

--- index.php
+++ index.php
@@ -12,8 +12,6 @@ namespace Bootgly;
 
 #error_reporting(E_ALL); ini_set('display_errors', 'On');
 
-const HOME_DIR = __DIR__.DIRECTORY_SEPARATOR;
-
 #phpinfo(); exit;
 
 require_once '@/autoload.php';
