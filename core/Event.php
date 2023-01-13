9e1368a0244321b20924db3fe8a3349a7bf2bc3c
Rodrigo Vieira <rodrigo@slayer.tech>
2023-01-13 09:35:05 -0300

    ➕ Added missing files

---
+++ core/Event.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * --------------------------------------------------------------------------
+ * Bootgly PHP Framework
+ * Developed by Rodrigo Vieira (@rodrigoslayertech)
+ * Copyright 2020-present
+ * Licensed under MIT
+ * --------------------------------------------------------------------------
+ */
+
+namespace Bootgly;
+
+
+class Event
+{}
