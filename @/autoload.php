22be252d093d61e3796f2c995bb71f533f24044e
Rodrigo Vieira <rodrigo@slayer.tech>
2023-01-30 07:37:54 -0300

    ✨ Bootgly HOME_BASE const

--- @/autoload.php
+++ @/autoload.php
@@ -3,16 +3,17 @@ namespace Bootgly;
 
 @include_once __DIR__ . '/imports/autoload.php';
 
-const HOME_DIR = __DIR__ . DIRECTORY_SEPARATOR . '../';
+define('HOME_BASE', rtrim(__DIR__, '/@'));
+const HOME_DIR = HOME_BASE . DIRECTORY_SEPARATOR;
 
 // TODO load with autoloader
-require_once __DIR__ . '/../boot/..php';
-require_once __DIR__ . '/../core/@loader.php';
-require_once __DIR__ . '/../interfaces/@loader.php';
+require_once HOME_BASE . '/boot/..php';
+require_once HOME_BASE . '/core/@loader.php';
+require_once HOME_BASE . '/interfaces/@loader.php';
 
-require_once __DIR__ . '/../nodes/@loader.php';
-require_once __DIR__ . '/../platforms/@loader.php';
+require_once HOME_BASE . '/nodes/@loader.php';
+require_once HOME_BASE . '/platforms/@loader.php';
 
 Bootgly::boot();
 
-require HOME_DIR . 'projects/bootgly.constructor.php';
+require HOME_BASE . '/projects/bootgly.constructor.php';
