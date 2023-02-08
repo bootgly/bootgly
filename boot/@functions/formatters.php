a3fc2d1c063c37301299a3df6dc4f595893c4c12
Rodrigo Vieira <rodrigo@slayer.tech>
2023-02-08 20:26:26 -0300

    🐛 formatBytes: Missing uncomment part of usable

--- boot/@functions/formatters.php
+++ boot/@functions/formatters.php
@@ -11,7 +11,7 @@ function formatBytes($bytes, $precision = 2) {
    $pow = \min($pow, \count($units) - 1); 
 
    // Uncomment one of the following alternatives
-   // $bytes /= pow(1024, $pow);
+   $bytes /= pow(1024, $pow);
    // $bytes /= (1 << (10 * $pow)); 
 
    return \round($bytes, $precision) . ' ' . $units[$pow]; 
