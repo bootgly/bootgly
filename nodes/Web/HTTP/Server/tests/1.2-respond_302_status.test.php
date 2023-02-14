e0760806336440a1480ea7ae8894c4b72ac19244
Rodrigo Vieira <rodrigo@slayer.tech>
2023-02-13 22:35:52 -0300

    ✅  Server Test: added response status test

---
+++ nodes/Web/HTTP/Server/tests/1.2-respond_302_status.test.php
@@ -0,0 +1,44 @@
+<?php
+use Bootgly\Bootgly;
+
+// SAPI
+use Bootgly\Web\HTTP\Server\Request;
+use Bootgly\Web\HTTP\Server\Response;
+use Bootgly\Web\HTTP\Server\Router;
+// CAPI?
+#use Bootgly\Web\HTTP\Client\Request;
+#use Bootgly\Web\HTTP\Client\Response;
+// TODO ?
+
+return [
+   // Server API
+   'sapi' => function (Request $Request, Response $Response, Router $Router) : Response {
+      return $Response->send(302); // 302 Not Found
+   },
+   // Client API
+   'capi' => function () {
+      // return $Request->get('/');
+      return "GET /status HTTP/1.0\r\n\r\n";
+   },
+
+   'assert' => function ($response) : bool {
+      /*
+      return $Response->status === '200 OK'
+      && $Response->body === 'Hello World!';
+      */
+
+      $expected = <<<HTML_RAW
+      HTTP/1.1 302 Found
+      Server: Bootgly\r
+      Content-Length: 0\r
+      Content-Type: text/html; charset=UTF-8
+      \n
+      HTML_RAW;
+
+      return $response === $expected;
+   },
+
+   'except' => function () : string {
+      return 'Status not matched';
+   }
+];
