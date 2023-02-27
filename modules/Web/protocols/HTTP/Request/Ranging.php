1fcef1623095fe5697198670ce65df0237c643be
Rodrigo Vieira <rodrigo@slayer.tech>
2023-02-27 12:38:06 -0300

    ✨ Added combineRanges to Request Ranging

--- modules/Web/protocols/HTTP/Request/Ranging.php
+++ modules/Web/protocols/HTTP/Request/Ranging.php
@@ -5,6 +5,9 @@
  * Developed by Rodrigo Vieira (@rodrigoslayertech)
  * Copyright 2020-present
  * Licensed under MIT
+ * 
+ * PHP version adapted from NPM range/parser:
+ * (https://github.com/jshttp/range-parser/blob/master/index.js)
  * --------------------------------------------------------------------------
  */
 
@@ -13,7 +16,7 @@ namespace Bootgly\Web\protocols\HTTP\Request;
 
 trait Ranging
 {
-   public function range (int $size, string $header, array $options = ['combine' => true])
+   public function range (int $size, string $header, bool $combine = false)
    {
       // @ Validate
       $equalIndex = strpos($header, '=');
@@ -25,7 +28,7 @@ trait Ranging
       $headerRanges = explode(',', substr($header, $equalIndex + 1));
       $ranges = [];
 
-      // @ Iterate ranges
+      // @ Iterate ranges (0-1,50-100,...)
       for ($i = 0; $i < count($headerRanges); $i++) {
          $range = explode('-', $headerRanges[$i]);
 
@@ -58,8 +61,64 @@ trait Ranging
          return -1; // Unsatisifiable range
       }
 
+      if ($combine) {
+         $ranges = $this->combineRanges($ranges);
+      }
+
       $ranges['type'] = substr($header, 0, $equalIndex);
 
       return $ranges;
+   }
+
+   private function combineRanges (array $ranges)
+   {
+      // @ Map with index
+      $ordered = array_map([$this, 'mapWithIndex'], $ranges, array_keys($ranges));
+      // @ Sort by range start
+      usort($ordered, function ($a, $b) {
+         return $a['start'] - $b['start'];
+      });
+  
+      for ($j = 0, $i = 1; $i < count($ordered); $i++) {
+         $next = &$ordered[$i];
+         $current = &$ordered[$j];
+
+         if ($next['start'] > $current['end'] + 1) {
+            // @ Next range
+            $ordered[++$j] = $next;
+         } else if ($next['end'] > $current['end']) {
+            // @ Extend range
+            $current['end'] = $next['end'];
+            $current['index'] = min($current['index'], $next['index']);
+         }
+      }
+
+      // @ Trim ordered array
+      $ordered2 = array_slice($ordered, 0, $j + 1);
+
+      // @ Generate combined range
+      // @ Sort by range index
+      usort($ordered2, function ($a, $b) {
+         return $a['index'] - $b['index'];
+      });
+      // @ Map without index
+      $combined = array_map([$this, 'mapWithoutIndex'], $ordered2);
+
+      return $combined;
+   }
+
+   private function mapWithIndex ($range, $index) {
+      return [
+         'start' => $range['start'],
+         'end' => $range['end'],
+         'index' => $index
+      ];
+   }
+
+   private function mapWithoutIndex ($range) {
+      return [
+         'start' => $range['start'],
+         'end' => $range['end']
+      ];
    }
 }
