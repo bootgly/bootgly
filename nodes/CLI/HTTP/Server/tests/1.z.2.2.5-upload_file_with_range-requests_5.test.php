7af7092c5ecce4816f640bcfa655630f26255f35
Rodrigo Vieira <rodrigo@slayer.tech>
2023-02-28 15:16:16 -0300

    ✅ HTTP Server CLI: added +1 test case \ - It should return the last 5 bytes of file when `Range: bytes=-5`

---
+++ nodes/CLI/HTTP/Server/tests/1.z.2.2.5-upload_file_with_range-requests_5.test.php
@@ -0,0 +1,68 @@
+<?php
+use Bootgly\Bootgly;
+use Bootgly\Debugger;
+// SAPI
+use Bootgly\CLI\HTTP\Server\Request;
+use Bootgly\CLI\HTTP\Server\Response;
+// CAPI?
+#use Bootgly\CLI\HTTP\Client\Request;
+#use Bootgly\CLI\HTTP\Client\Response;
+// TODO ?
+
+return [
+   // @ arrange
+   'describe' => 'It should return the last 5 bytes of file when `bytes=-5`',
+
+   // @ act
+   // Server API
+   'sapi' => function (Request $Request, Response $Response) : Response {
+      Bootgly::$Project->vendor = '@bootgly/';
+      Bootgly::$Project->container = 'web/';
+      Bootgly::$Project->package = 'examples/';
+      Bootgly::$Project->version = 'app/';
+
+      Bootgly::$Project->setPath();
+
+      return $Response('statics/alphanumeric.txt')->upload(close: false);
+   },
+   // Client API
+   'capi' => function ($host) {
+      $raw = <<<HTTP_RAW
+      GET /test/download/file_with_range/one_range/5 HTTP/1.1\r
+      Host: {$host}\r
+      User-Agent: Bootgly\r
+      Range: bytes=-5\r
+      \r\n
+      HTTP_RAW;
+
+      return $raw;
+   },
+
+   // @ assert
+   'assert' => function ($response) : bool {
+      $expected = <<<HTML_RAW
+      HTTP/1.1 206 Partial Content\r
+      Server: Bootgly\r
+      Content-Length: 5\r
+      Content-Range: bytes 57-61/62\r
+      Content-Type: application/octet-stream\r
+      Content-Disposition: attachment; filename="alphanumeric.txt"\r
+      Last-Modified: Tue, 28 Feb 2023 15:30:43 GMT\r
+      Cache-Control: no-cache, must-revalidate\r
+      Expires: 0\r
+      \r
+      56789
+      HTML_RAW;
+
+      if (substr($response, 0, 363) !== $expected) {
+         Debugger::$labels = ['HTTP Response:', 'Expected:'];
+         debug(json_encode(substr($response, 0, 363)), json_encode($expected));
+         return false;
+      }
+
+      return true;
+   },
+   'except' => function () : string {
+      return 'Response did not return the entire contents of the file?';
+   }
+];
