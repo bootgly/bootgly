dbf4f71e6eb46d3cbfde9a7f76df77c693b46565
Rodrigo Vieira <rodrigo@slayer.tech>
2023-03-17 10:03:43 -0300

    📝 Added CLI - Table - Example #1

---
+++ projects/@bootgly/cli/examples/components/table-example-1.php
@@ -0,0 +1,48 @@
+<?php
+
+use Bootgly\CLI\Terminal\components\Table;
+
+
+$Table = new Table($Output);
+
+$Table->Data->set(header: ['Alura Courses', 'Quantity']);
+$Table->Data->set(body: [
+   ['Programação',       280], // using multibyte text
+   ['Front-end',         112],
+   ['Data Science',      209],
+   ['DevOps',            135],
+   ['@---;'],                  // That's a row separator!
+   ['UX & Design',       276],
+   ['Mobile',            93],
+   ['Inovação & Gestão', 297], // using multibyte text
+]);
+$Table->Data->set(footer: [
+   'Total:',  $Table->Data->sum(column: 1)
+]);
+
+$Output->write("\n");
+
+$loops = 3;
+for ($i = 1; $i <= $loops; $i++) {
+   $Output->Cursor->moveTo(3, 1);
+
+   switch ($i) {
+      case 1:
+         $alignment = 'left';
+         break;
+      case 2:
+         $alignment = 'center';
+         break;
+      case 3:
+         $alignment = 'right';
+         break;
+   };
+
+   // ! Cells
+   $Table->Cells->align($alignment);
+   $Output->append('Cells - text aligment: ' . $alignment . '   ');
+
+   $Table->render();
+
+   sleep(1);
+}
