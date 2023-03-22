44ff5b72baf17dfd9a3f2e59112329de32903a20
Rodrigo Vieira <rodrigo@slayer.tech>
2023-03-22 10:45:39 -0300

    📝 CLI examples: added text modifying example 1

---
+++ projects/@bootgly/cli/examples/terminal/output_text-modifying-01.example.php
@@ -0,0 +1,42 @@
+<?php
+namespace Bootgly\CLI;
+
+use Bootgly\CLI;
+
+$Output = CLI::$Terminal->Output;
+$Output->reset();
+$Output->waiting = 30000;
+
+$Output->Text->colorize('green');
+$Output->write(<<<OUTPUT
+*
+* Bootgly Terminal Output - Text Modifying - Example #1
+*\n\n
+OUTPUT);
+$Output->Text->colorize();
+
+$Output->writing("|Inserting 3 spaces at the current cursor position...\n");
+$Output->Cursor->up(1);
+$Output->Cursor->moveTo(column: 1);
+
+$Output->Text->space(3);
+
+$Output->Cursor->down(2);
+
+
+$Output->writing("|Deleting 3 characters at the current cursor position...\n");
+$Output->Cursor->up(1);
+$Output->Cursor->moveTo(column: 1);
+
+$Output->Text->delete(3);
+
+$Output->Cursor->down(2);
+
+
+$Output->writing("|Erasing 3 characters from the current cursor position...\n");
+$Output->Cursor->up(1);
+$Output->Cursor->moveTo(column: 1);
+
+$Output->Text->erase(3);
+
+$Output->Cursor->down(2);
