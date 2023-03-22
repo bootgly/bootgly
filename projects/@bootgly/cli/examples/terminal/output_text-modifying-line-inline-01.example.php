b04097599bce412b578164734326415d31d1f542
Rodrigo Vieira <rodrigo@slayer.tech>
2023-03-22 14:37:25 -0300

    📝 CLI examples: added text line inline example 1

---
+++ projects/@bootgly/cli/examples/terminal/output_text-modifying-line-inline-01.example.php
@@ -0,0 +1,33 @@
+<?php
+namespace Bootgly\CLI;
+
+use Bootgly\CLI;
+
+$Output = CLI::$Terminal->Output;
+$Output->reset();
+$Output->waiting = 100000; // @ Wait time in miliseconds to "writing" (per character written)
+
+$Output->Text->colorize('green');
+$Output->write(<<<OUTPUT
+*
+* Bootgly Terminal Output - Text Modifying - Line - Inline - Example #1
+*\n\n
+OUTPUT);
+$Output->Text->colorize();
+
+$Output->writing("Trim all text to the right of cursor... ->!@#$%^&");
+$Output->Cursor->left(7);
+
+sleep(3);
+
+$Output->Text->trim(right: true);
+
+$Output->Cursor->down(1)->Cursor->moveTo(column: 1);
+
+$Output->writing("!@#$%^&<- Trim all text to the left of cursor...");
+$Output->Cursor->moveTo(column: 7);
+
+sleep(3);
+
+$Output->Text->trim(left: true, right: false);
+$Output->write("\n");
