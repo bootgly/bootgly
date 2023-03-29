6220869d4c2680ad38e8a6a923a81ee32b82c0da
Rodrigo Vieira <rodrigo@slayer.tech>
2023-03-29 09:46:58 -0300

    🆕 Introduce Terminal Input reading method: \ * Create a Client and Server API to: - read and write input in Client Side; - reading input in Server Side;
    * Input is in non-blocking mode
    
    🚀🤯

--- interfaces/CLI/Terminal/Input.php
+++ interfaces/CLI/Terminal/Input.php
@@ -11,23 +11,98 @@
 namespace Bootgly\CLI\Terminal;
 
 
-use Bootgly\CLI;
+use Bootgly\Pipe;
 
 
 class Input
 {
+   // * Config
+   // ...
+
    // * Data
    public $stream;
+
+   // * Meta
+   // ...
 
 
    public function __construct ($stream = STDIN)
    {
+      // * Config
+      // ...
+
       // * Data
       $this->stream = $stream;
+
+      // * Meta
+      // ...
    }
 
-   public function read (int $length)
+   public function read (int $length) : string|false
    {
-      return fread($this->stream, $length);
+      pcntl_signal_dispatch();
+
+      try {
+         $read = @fread($this->stream, $length);
+      } catch (\Throwable) {
+         $read = false;
+      }
+
+      if ($read === false) {
+         // TODO check errors
+      }
+
+      return $read;
+   }
+
+   public function reading (\Closure $CAPI, \Closure $SAPI)
+   {
+      $Pipe = new Pipe(timeout: 10);
+
+      $stream = $this->stream;
+
+      // @ Register shutdown function
+      register_shutdown_function(function () use ($stream) {
+         // Set blocking for data stream
+         stream_set_blocking($stream, true);
+         // Restore terminal settings
+         system('stty icanon echo');
+      });
+
+      // @ Fork process
+      $pid = pcntl_fork();
+
+      if ($pid === 0) { // @ Child (Client)
+         cli_set_process_title("BootglyCLI: child process");
+
+         // Watch for a signal from the parent process to terminate
+         pcntl_signal(SIGTERM, function () {
+            exit(0);
+         });
+
+         // Configure terminal
+         system('stty -icanon -echo');
+         // Set non-blocking for data stream
+         stream_set_blocking($this->stream, false);
+
+         // @ Call Terminal Client API passing the Pipe write method
+         $CAPI([$this, 'read'], [$Pipe, 'write']);
+
+         // Close Client API
+         exit(0);
+      } else if ($pid > 0) { // @ Parent (Server)
+         cli_set_process_title("BootglyCLI: master process");
+
+         // @ Call Terminal Server API passing the Pipe reading method
+         $SAPI([$Pipe, 'reading']);
+
+         // Send signal to terminate child process
+         posix_kill($pid, SIGTERM);
+
+         // Wait for child process to exit
+         pcntl_waitpid($pid, $status);
+      } else if ($pid === -1) {
+         die('Could not fork process!');
+      }
    }
 }
