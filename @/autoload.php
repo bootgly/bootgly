1778006cebfaba789def55879befa33273b7053b
Rodrigo Vieira <rodrigo@slayer.tech>
2023-03-31 18:56:26 -0300

    🌱 Use include_once in loader

--- @/autoload.php
+++ @/autoload.php
@@ -9,13 +9,13 @@ const HOME_DIR = HOME_BASE . DIRECTORY_SEPARATOR;
 
 // TODO load with autoloader
 // @ Bootstrap
-require_once HOME_BASE . '/abstract/..php';
-require_once HOME_BASE . '/base/@loader.php';
-require_once HOME_BASE . '/core/@loader.php';
+@include_once HOME_BASE . '/abstract/..php';
+@include_once HOME_BASE . '/base/@loader.php';
+@include_once HOME_BASE . '/core/@loader.php';
 // @ Resources
-require_once HOME_BASE . '/interfaces/@loader.php';
-require_once HOME_BASE . '/modules/@loader.php';
-require_once HOME_BASE . '/nodes/@loader.php';
-require_once HOME_BASE . '/platforms/@loader.php';
+@include_once HOME_BASE . '/interfaces/@loader.php';
+@include_once HOME_BASE . '/modules/@loader.php';
+@include_once HOME_BASE . '/nodes/@loader.php';
+@include_once HOME_BASE . '/platforms/@loader.php';
 
 Bootgly::boot();
