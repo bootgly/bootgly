06f5fc36e64a8d7b25f9528e5514792b00c46eb7
Rodrigo Vieira <rodrigo@slayer.tech>
2023-04-10 11:38:33 -0300

    📚 Bootgly CLI Examples: Output Alert example 01

---
+++ projects/@bootgly/cli/examples/terminal/Output/components/Alert-01.example.php
@@ -0,0 +1,27 @@
+<?php
+namespace Bootgly\CLI;
+
+use Bootgly\CLI;
+use Bootgly\CLI\Terminal\components\Alert;
+
+$Output = CLI::$Terminal->Output;
+$Output->reset();
+
+$Output->render(<<<OUTPUT
+/* @*:
+ * @#green: Bootgly CLI Terminal - Alert component @;
+ * @#yellow: @@ Demo - Example #1 @;
+ * {$location}
+ */\n\n
+OUTPUT);
+
+$Alert = new Alert($Output);
+
+$Alert->Type::SUCCESS->set();
+$Alert->emit('Success message.');
+
+$Alert->Type::ATTENTION->set();
+$Alert->emit('Attention message.');
+
+$Alert->Type::FAILURE->set();
+$Alert->emit('Failure message.');
