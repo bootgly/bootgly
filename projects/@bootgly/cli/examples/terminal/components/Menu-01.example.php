8ce858de59fb878bf2d3173df06b4403698751b0
Rodrigo Vieira <rodrigo@slayer.tech>
2023-04-11 21:36:28 -0300

    📚 Bootgly CLI - added Menu component example 1

---
+++ projects/@bootgly/cli/examples/terminal/components/Menu-01.example.php
@@ -0,0 +1,58 @@
+<?php
+namespace Bootgly\CLI;
+
+use Bootgly\CLI;
+use Bootgly\CLI\Terminal\components\Menu\ {
+   Menu
+};
+
+$Input = CLI::$Terminal->Input;
+$Output = CLI::$Terminal->Output;
+$Output->reset();
+
+$Output->render(<<<OUTPUT
+/* @*:
+ * @#green: Bootgly CLI Terminal - Menu component @;
+ * @#yellow: @@ Demo - Example #1 @;
+ * {$location}
+ */\n\n
+OUTPUT);
+
+$Menu = new Menu($Input, $Output);
+
+$Menu->title = "Choose one or more options:\n";
+// > Items
+$Items = $Menu->Items;
+// * Config
+// @ Selection
+$Items->Selection::Multiple->set();
+$Items->selectable = true;
+$Items->deselectable = true;
+// @ Display
+$Items->Orientation::Vertical->set();
+
+
+/*
+$Items->set([
+   'Option 1' => [
+      'prepend' => '@#red: ',
+      'append' => ' @;'
+   ],
+   '@#blue: Option 2 @;',
+   'Option 3'
+]);
+*/
+$Items
+   ->add('Option 1')
+   ->add('Option 2')
+   ->add('Option 3');
+
+$selected = $Menu->open();
+
+
+echo "\n";
+if ( ! empty($selected) ) {
+   $Output->write("Selected options indexes: " . implode(", ", $selected) . PHP_EOL);
+} else {
+   echo "No options selected." . PHP_EOL;
+}
