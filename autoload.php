f896fbee891f2f6529bd4ea1a6fca3395cc677fc
Rodrigo Vieira <rodrigo@slayer.tech>
2023-05-10 19:48:03 -0300

    🚚 Prepare to reorganize the directory structure

--- autoload.php
+++ autoload.php
@@ -6,19 +6,19 @@ define('BOOTGLY_HOME_DIR', __DIR__ . DIRECTORY_SEPARATOR);
 
 // TODO load with autoloader
 // @ Bootables
-@include_once BOOTGLY_HOME_BASE . '/Bootgly/abstract/loader.php';
-@include_once BOOTGLY_HOME_BASE . '/Bootgly/base/loader.php';
-@include_once BOOTGLY_HOME_BASE . '/Bootgly/core/loader.php';
+@include_once BOOTGLY_HOME_BASE . '/Bootgly/-abstract/loader.php';
+@include_once BOOTGLY_HOME_BASE . '/Bootgly/-base/loader.php';
+@include_once BOOTGLY_HOME_BASE . '/Bootgly/-core/loader.php';
 // @ Features
-@include_once BOOTGLY_HOME_BASE . '/Bootgly/interfaces/loader.php';
-@include_once BOOTGLY_HOME_BASE . '/Bootgly/modules/loader.php';
-@include_once BOOTGLY_HOME_BASE . '/Bootgly/nodes/loader.php';
-@include_once BOOTGLY_HOME_BASE . '/Bootgly/platforms/loader.php';
+@include_once BOOTGLY_HOME_BASE . '/Bootgly/-interfaces/loader.php';
+@include_once BOOTGLY_HOME_BASE . '/Bootgly/-modules/loader.php';
+@include_once BOOTGLY_HOME_BASE . '/Bootgly/-nodes/loader.php';
+@include_once BOOTGLY_HOME_BASE . '/Bootgly/-platforms/loader.php';
 // @ Workables
 // composer?
 $installed = BOOTGLY_HOME_BASE . '/../../composer/installed.php';
 if ( is_file($installed) ) {
-   $installed = @include @$installed;
+   $installed = @include $installed;
 
    $root = $installed['root']['install_path'] ?? null;
    if ($root) {
