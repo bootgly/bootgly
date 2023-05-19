a9412589650375e0c0e6e89a91178bd37896895b
Rodrigo Vieira <rodrigo@slayer.tech>
2023-05-19 19:54:10 -0300

    🔨 Updated CLI Terminal Input Mouse report method

--- Bootgly/CLI/Terminal/Input/Mouse.php
+++ Bootgly/CLI/Terminal/Input/Mouse.php
@@ -21,10 +21,21 @@ class Mouse
    use Reportable;
 
 
-   public function report (bool $click)
+   public function report (bool $clicks, bool $movements)
    {
-      return match($click) {
-         true => self::_START_ESCAPE . self::_MOUSE_ENABLE_CLICK_REPORTING,
+      $any = $clicks && $movements;
+
+      match ($any) {
+         true  => self::_START_ESCAPE . self::_MOUSE_ENABLE_ALL_EVENT_REPORTING,
+         false => self::_START_ESCAPE . self::_MOUSE_DISABLE_ALL_EVENT_REPORTING
+      };
+
+      if ($any) {
+         return;
+      }
+
+      match ($clicks) {
+         true  => self::_START_ESCAPE . self::_MOUSE_ENABLE_CLICK_REPORTING,
          false => self::_START_ESCAPE . self::_MOUSE_DISABLE_CLICK_REPORTING
       };
    }
