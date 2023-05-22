63a6befb0909986d54dab862f255e2d861c7aca0
Rodrigo Vieira <rodrigo@slayer.tech>
2023-05-22 19:06:09 -0300

    🚧 WIP new Project methods

--- Bootgly/-core/Project.php
+++ Bootgly/-core/Project.php
@@ -13,6 +13,10 @@ namespace Bootgly;
 
 class Project
 {
+   // ! Path
+   // Bootgly Project (Author)
+   public const BOOTGLY_PROJECT_DIR = BOOTGLY_BASE . '/projects/';
+   // Project (Consumer)
    public const PROJECT_DIR = BOOTGLY_WORKABLES_BASE . '/project/';
    public const PROJECTS_DIR = BOOTGLY_WORKABLES_BASE . '/projects/';
 
@@ -23,36 +27,58 @@ class Project
    public string $vendor;       // @bootgly/
    public string $container;    // examples/
    public string $package;      // app/
-
+   // ---
    public string $type;         // spa, pwa, etc.
-
    public string $public;       // dist/
-
    public string $version;      // v1/
 
    // * Meta
-   public static string $path0;
+   private static array $projects;
+   private static string $selected;
+   #private string $path;
    private array $paths;
 
 
-   public function __construct ()
+   public function __construct (? string $path = null)
    {
+      // ! Path
       // * Config
       $this->cache = true;
 
       // * Data
+      // TODO templates
       $this->vendor = '';
       $this->container = '';
       $this->package = '';
-
+      // ---
       $this->type = '';
-
       $this->public = '';
-
       $this->version = '';
 
       // * Meta
+      self::$projects = [];
+      self::$selected = '';
       $this->paths = [];
+
+      // @
+      if ($path) {
+         $path = trim($path, '/');
+
+         $paths = explode('/', $path);
+
+         foreach ($paths as $index => $path) {
+            match ($index) {
+               0 => $this->vendor    = $path . '/',
+               1 => $this->container = $path . '/',
+               2 => $this->package   = $path . '/',
+               3 => $this->type      = $path . '/',
+               4 => $this->public    = $path . '/',
+               5 => $this->version   = $path . '/'
+            };
+         }
+
+         #$this->path = PROJECTS_DIR . $path;
+      }
    }
 
    public function __get ($name)
@@ -74,6 +100,76 @@ class Project
       return $this->path;
    }
 
+   // ! Path
+   public function construct () : string
+   {
+      $path = self::PROJECTS_DIR;
+
+      // @ 1 - Construct path
+      $path .= $this->vendor ?? $this->name;
+      $path .= $this->container;
+      $path .= $this->package;
+
+      $path .= $this->type;
+
+      $path .= $this->public;
+
+      $path .= $this->version;
+
+      // @ Fix constructed path
+      $path = trim($path, '/');
+      $path .= '/';
+
+      // @ Push constructed path to project paths
+      $this->paths[] = $path;
+
+      return $path;
+   }
+   public function save (? string $backup = null) : bool
+   {
+      $key = $this->path;
+      $path = $backup ?? $key;
+
+      $project = self::$projects[$key] ?? null;
+
+      if ($project === null) {
+         $path = $this->construct();
+
+         // @ Save path to projects list
+         self::$projects[$key][] = $path;
+
+         // @ Set path to selected
+         self::$selected = $key;
+
+         return true;
+      }
+
+      return false;
+   }
+
+   public function get (string $version = '@') : string
+   {
+      $project = self::$projects[self::$selected];
+
+      // @ Get project path version
+      $path = $project[$version] ?? '';
+
+      return $path;
+   }
+
+   public function select (string $name) : bool
+   {
+      $project = self::$projects[$name] ?? null;
+
+      if ($project === null) {
+         return false;
+      }
+
+      self::$selected = $name;
+
+      return true;
+   }
+   // DEPRECATED
    public function setPath ()
    {
       // Join paths
@@ -89,10 +185,7 @@ class Project
 
       $path .= $this->version;
 
-      // Set static paths[0] (Main path)
-      self::$path0 = $path;
-
-      // Push to paths array
+      // @ Push to paths array
       $this->paths[] = $path;
    }
 }
