299e1c8a7a2e664a17286bb92f2dab09d3461b21
Rodrigo Vieira <rodrigo@slayer.tech>
2023-05-24 12:33:53 -0300

    ✅ Added +asserts to Project test 1

--- Bootgly/-core/tests/1.Project-1.test.php
+++ Bootgly/-core/tests/1.Project-1.test.php
@@ -14,29 +14,41 @@ return [
       $expected1 = Project::PROJECTS_DIR . 'Bootgly/';
       $expected2 = Project::PROJECTS_DIR . 'Bootgly/CLI/';
 
+      // ! Project 1 instance
       $Project1 = new Project;
       // @ Construct
-      $path = $Project1->construct('Bootgly');
-      assert($path === $expected1, 'Failed to construct Project path');
+      $path1 = $Project1->construct('Bootgly');
+      assert($path1 === $expected1, 'Failed to construct Project path 1');
       // @ Name
-      $Project1->name('Calopsita');
+      $Project1->name('Bootgly');
       // @ Save
-      $path = $Project1->save();
-      assert($path === $expected1, 'Failed to save Project path');
+      $path1 = $Project1->save();
+      assert($path1 === $expected1, 'Failed to save Project path 1');
       // @ Get
-      $path = $Project1->get(path: 0);
-      assert($path === $expected1, 'Failed to get Project path');
+      $path1 = $Project1->get(path: 0);
+      assert($path1 === $expected1, 'Failed to get Project path 1');
       // ---
       // @ Construct
       $Project1->construct('Bootgly/CLI');
       // @ Get
-      $path = $Project1->get(path: 1);
-      assert($path === $expected2, 'Failed to get Project path 2');
+      $path2 = $Project1->get(path: 1);
+      assert($path2 === $expected2, 'Failed to get Project path 2');
 
-      // @ Select
+      // ! Project 2 instance
       $Project2 = new Project;
-      $path2 = $Project2->select(project: 0);
-      assert($path2 === $expected1, 'Failed to select Project path 2');
+      // @ Select
+      $path1 = $Project2->select(project: 0);
+      assert($path1 === $expected1, 'Failed to select Project path 1 by index');
+      // @ Construct
+      $Project2->construct('Bootgly/CLI');
+      // @ Name
+      $Project2->name('BootglyCLI');
+      // @ Save
+      $path2 = $Project2->save();
+      assert($path2 === $expected2, 'Failed to save Project path 2');
+      // @ Select
+      $path2 = $Project2->select(project: 'BootglyCLI');
+      assert($path2 === $expected2, 'Failed to select Project path 2 by name');
 
       return true;
    }
