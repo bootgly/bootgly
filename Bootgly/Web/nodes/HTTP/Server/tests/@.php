8185d602bcaebd03f1c9d09bbd50b544000596e8
Rodrigo Vieira <rodrigo@slayer.tech>
2023-07-14 11:33:33 -0300

    ✅ Added +1 test to Bootgly HTTP Server CLI: \ 🆕 If-None-Match (Request) with ETag (Response)

--- Bootgly/Web/nodes/HTTP/Server/tests/@.php
+++ Bootgly/Web/nodes/HTTP/Server/tests/@.php
@@ -66,6 +66,7 @@ return [
          '2.1.3-request_cache-if-modified-since',
          '2.1.4-request_cache-if-modified-since',
          '2.1.5-request_cache-if-modified-since',
+         '2.2.1-request_cache-if-none-match',
       ],
       'Response/' => [
          '1.1-respond_with_a_simple_hello_world',
