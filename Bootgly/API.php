ba0bd2add5d60828b42d0b9031618a898f8e5f19
Rodrigo Vieira <rodrigo@slayer.tech>
2023-07-23 18:04:04 -0300

    🌱 Added missing interface classes

---
+++ Bootgly/API.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * --------------------------------------------------------------------------
+ * Bootgly PHP Framework
+ * Developed by Rodrigo Vieira (@rodrigoslayertech)
+ * Copyright 2023-present
+ * Licensed under MIT
+ * --------------------------------------------------------------------------
+ */
+
+namespace Bootgly;
+
+
+class API // Application Programming Interface (from Bootgly itself)
+{
+   
+}
