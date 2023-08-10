4d53f1eab3d413f10b27fe36c26e20abf70a86c8
Rodrigo Vieira <rodrigo@slayer.tech>
2023-08-10 19:23:02 -0300

    🚧 Added tests templates files (WIP)

---
+++ Bootgly/ACI/Tests/templates/@.php
@@ -0,0 +1,12 @@
+<?php
+return [
+   // * Config
+   'autoBoot' => __DIR__,
+   'autoInstance' => true,
+   'autoResult' => true,
+   'autoSummarize' => true,
+   'exitOnFailure' => true,
+   // * Data
+   'suiteName' => '...',
+   'tests' => []
+];
---
+++ Bootgly/ACI/Tests/templates/closure-tests.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// * put uses here *
+
+
+return [
+   // @ configure
+   'describe' => '',
+   // @ simulate
+   // ...
+   // @ test
+   'test' => function () {
+      // ...
+
+      return true;
+   }
+];
