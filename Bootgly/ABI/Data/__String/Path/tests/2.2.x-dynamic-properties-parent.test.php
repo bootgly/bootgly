063a6e29d296be8a22c4d10e2e07b95e2d2fd2d4
Rodrigo Vieira <rodrigo@slayer.tech>
2023-09-28 20:21:10 -0300

    ✅ Added assertion descriptions

--- Bootgly/ABI/Data/__String/Path/tests/2.2.x-dynamic-properties-parent.test.php
+++ Bootgly/ABI/Data/__String/Path/tests/2.2.x-dynamic-properties-parent.test.php
@@ -14,19 +14,19 @@ return [
       // @
       // ! Dir
       // ? Absolute
-      // Valid - absolute dir (2 parts)
+      self::$description = 'Valid - absolute dir (2 parts)';
       $Path = new Path('/etc/php/');
       yield assert(
          assertion: $Path->parent === '/etc/',
          description: 'Path #11 - parent: ' . $Path->parent
       );
-      // Valid - absolute dir (1 part)
+      self::$description = 'Valid - absolute dir (1 part)';
       $Path = new Path('/etc/');
       yield assert(
          assertion: $Path->parent === '/',
          description: 'Path #12 - parent: ' . $Path->parent
       );
-      // Valid - absolute dir (0 part)
+      self::$description = 'Valid - absolute dir (0 part)';
       $Path = new Path('/');
       yield assert(
          assertion: $Path->parent === '/',
@@ -34,19 +34,19 @@ return [
       );
 
       // ? Relative
-      // Valid - relative dir (2 parts)
+      self::$description = 'Valid - relative dir (2 parts)';
       $Path = new Path('etc/php/');
       yield assert(
          assertion: $Path->parent === 'etc/',
          description: 'Path #21 - parent: ' . $Path->parent
       );
-      // Valid - relative dir (1 part)
+      self::$description = 'Valid - relative dir (1 part)';
       $Path = new Path('etc/');
       yield assert(
          assertion: $Path->parent === './', // Valid?
          description: 'Path #22 - parent: ' . $Path->parent
       );
-      // Invalid - relative dir (0 part)
+      self::$description = 'Invalid - relative dir (0 part)';
       $Path = new Path('');
       yield assert(
          assertion: $Path->parent === '',
@@ -56,25 +56,25 @@ return [
 
       // ! File
       // ? Absolute
-      // Valid - absolute file (2 parts)
+      self::$description = 'Valid - absolute file (2 parts)';
       $Path = new Path('/var/test.php');
       yield assert(
          assertion: $Path->parent === '/var/',
          description: 'Path #31 - parent: ' . $Path->parent
       );
-      // Valid - absolute file (1 part)
+      self::$description = 'Valid - absolute file (1 part)';
       $Path = new Path('/test.php');
       yield assert(
          assertion: $Path->parent === '/',
          description: 'Path #32 - parent: ' . $Path->parent
       );
-      // Valid - absolute file without extension (2 parts)
+      self::$description = 'Valid - absolute file without extension (2 parts)';
       $Path = new Path('/var/test');
       yield assert(
          assertion: $Path->parent === '/var/',
          description: 'Path #33 - parent: ' . $Path->parent
       );
-      // Valid - absolute file without extension (1 part)
+      self::$description = 'Valid - absolute file without extension (1 part)';
       $Path = new Path('/test');
       yield assert(
          assertion: $Path->parent === '/',
