3c713aa81ec25e0bef14dd9b728de8a2ba1dac3d
Rodrigo Vieira <rodrigo@slayer.tech>
2023-10-12 11:55:46 -0300

    🚸 🧑‍💻 Debugging - Support to report Error

--- Bootgly/ABI/Debugging/Errors.php
+++ Bootgly/ABI/Debugging/Errors.php
@@ -38,8 +38,66 @@ abstract class Errors
       return true;
    }
 
-   public static function dump (...$data)
+   public static function report (\Error $Error)
    {
-      // ...
+      // @ Output
+      $output = "\n";
+      // class
+      $output .= match (\PHP_SAPI) {
+         'cli' => "\033[0;30;41m ",
+         default => ''
+      };
+      $output .= get_class($Error);
+      $output .= match (\PHP_SAPI) {
+         'cli' => " \033[0m",
+         default => ''
+      };
+      // code
+      $output .= match (\PHP_SAPI) {
+         'cli' => "\033[47;30m ",
+         default => ''
+      };
+      $output .= "#";
+      $output .= $Error->getCode();
+      $output .= match (\PHP_SAPI) {
+         'cli' => " \033[0m\n\n",
+         default => ''
+      };
+      // message
+      $output .= match (\PHP_SAPI) {
+         'cli' => "\033[97m ",
+         default => ''
+      };
+      $output .= $Error->getMessage();
+      $output .= match (\PHP_SAPI) {
+         'cli' => " \033[0m\n\n",
+         default => ''
+      };
+      // file
+      $output .= " at ";
+      $output .= match (\PHP_SAPI) {
+         'cli' => "\033[92m",
+         default => ''
+      };
+      $output .= $Error->getFile();
+      $output .= match (\PHP_SAPI) {
+         'cli' => "\033[0m",
+         default => ''
+      };
+      // file line
+      $output .= match (\PHP_SAPI) {
+         'cli' => ":\033[96m",
+         default => ''
+      };
+      $output .= $Error->getLine();
+      $output .= match (\PHP_SAPI) {
+         'cli' => "\033[0m\n\n",
+         default => ''
+      };
+      $output .= "\n";
+      // file content
+      // TODO
+
+      echo $output;
    }
 }
