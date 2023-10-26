63f69b2da334694fd9fd075b9c6c067c9b0646c4
Rodrigo Vieira <rodrigo@slayer.tech>
2023-10-26 18:05:06 -0300

    🆕 CLI Terminal component - new Field component

---
+++ Bootgly/CLI/Terminal/components/Field/Field.php
@@ -0,0 +1,143 @@
+<?php
+/*
+ * --------------------------------------------------------------------------
+ * Bootgly PHP Framework
+ * Developed by Rodrigo Vieira (@rodrigoslayertech)
+ * Copyright 2023-present
+ * Licensed under MIT
+ * --------------------------------------------------------------------------
+ */
+
+namespace Bootgly\CLI\Terminal\components\Field;
+
+
+use Bootgly\CLI\Terminal\Output;
+
+
+class Field
+{
+   private Output $Output;
+
+   // * Config
+   public ? string $title;
+   // @ Style
+   public string $color;
+   public const DEFAULT_BORDERS = [
+      'top'          => '─',
+      'top-left'     => '┌',
+      'top-right'    => '┐',
+
+      'bottom'       => '─',
+      'bottom-left'  => '└',
+      'bottom-right' => '┘',
+
+      'left'         => '│',
+      'right'        => '│',
+   ];
+   public array $borders;
+   // * Data
+   protected ? string $content;
+
+
+   public function __construct (Output $Output)
+   {
+      $this->Output = $Output;
+
+      // * Config
+      $this->title = null;
+      // @ Style
+      $this->color = '@#Black:';
+      $this->borders = self::DEFAULT_BORDERS;
+      // * Data
+      $this->content = null;
+   }
+   public function __set ($name, $value)
+   {
+      match ($name) {
+         'content' => $this->content = $value, // TODO emit event
+         default => null
+      };
+   }
+
+   public function border (string $position, ? int $length = null)
+   {
+      $Output = $this->Output;
+
+      $color = $this->color;
+      $borders = $this->borders;
+
+      switch ($position) {
+         case 'top':
+            $title = " $this->title ";
+
+            $border_top_left = $borders['top-left'];
+            $border_top_right = $borders['top-right'];
+
+            $line = str_repeat('─', $length);
+
+            $Output->render(<<<OUTPUT
+            {$color}{$border_top_left}{$title}{$line}{$border_top_right}@;\n
+            OUTPUT);
+
+            break;
+         case 'left':
+            $border_left = $borders['left'];
+
+            $Output->render(<<<OUTPUT
+            {$color}{$border_left}@; 
+            OUTPUT);
+
+            break;
+         case 'right':
+            $border_right = $borders['right'];
+
+            $Output->render(<<<OUTPUT
+             {$color}{$border_right}@;\n
+            OUTPUT);
+
+            break;
+         case 'bottom':
+            $border_bottom_left = $borders['bottom-left'];
+            $border_bottom = $borders['bottom'];
+            $border_bottom_right = $borders['bottom-right'];
+
+            $line = str_repeat($border_bottom, $length);
+
+            $Output->render(<<<OUTPUT
+            {$color}{$border_bottom_left}{$line}{$border_bottom_right}\n
+            OUTPUT);
+            break;
+      }
+   }
+   public function render ()
+   {
+      $Output = $this->Output;
+      $Text = $Output->Text;
+
+      // * Config
+      $title_length = mb_strlen($this->title);
+      // * Meta
+      $content_lines = explode(PHP_EOL, $this->content);
+
+      // @ Determine max line length (based on content line and title)
+      $line_length = $title_length;
+      foreach ($content_lines as $content_line) {
+         $content_line_length = mb_strlen($content_line);
+         if ($content_line_length > $line_length) {
+            $line_length = $content_line_length;
+         }
+      }
+
+      $this->border('top', $line_length - $title_length);
+      // @ Render content lines
+      foreach ($content_lines as $content_line) {
+         $this->border('left');
+         $Output->pad($content_line, $line_length);
+         $this->border('right');
+      }
+      $this->border('bottom', $line_length + 2);
+
+      // @ Reset Text
+      $Text->stylize('reset');
+   }
+}
