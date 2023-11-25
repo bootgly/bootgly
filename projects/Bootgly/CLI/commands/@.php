24131e6f5496f3540aa1ea1d14f03cc43dcd8de2
Rodrigo Vieira <rodrigo@slayer.tech>
2023-11-24 21:23:35 -0300

    🚸🧑‍💻 Introduce CLI boot Command

--- projects/Bootgly/CLI/commands/@.php
+++ projects/Bootgly/CLI/commands/@.php
@@ -1,11 +1,11 @@
 <?php
 namespace projects\Bootgly\CLI\commands;
 
-
 return [
    new DemoCommand,
    new SetupCommand,
 
+   new BootCommand,
    new TestCommand,
 
    new ProjectCommand,
---
+++ projects/Bootgly/CLI/commands/BootCommand.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * --------------------------------------------------------------------------
+ * Bootgly PHP Framework
+ * Developed by Rodrigo Vieira (@rodrigoslayertech)
+ * Copyright 2020-present
+ * Licensed under MIT
+ * --------------------------------------------------------------------------
+ */
+
+namespace projects\Bootgly\CLI\commands;
+
+
+use function Bootgly\ABI\copy_recursively;
+
+use Bootgly\CLI;
+use Bootgly\CLI\Command;
+use Bootgly\CLI\UI\Alert\Alert;
+
+
+class BootCommand extends Command
+{
+   // * Config
+   public int $group = 1;
+
+   public string $name = 'boot';
+   public string $description = 'Boot Bootgly resources, projects, etc.';
+
+
+   public function run (array $arguments, array $options) : bool
+   {
+      if ($options['resources']) {
+         $Output = CLI::$Terminal->Output;
+         $Alert = new Alert($Output);
+
+         if (BOOTGLY_ROOT_DIR === BOOTGLY_WORKING_DIR) {
+            $Alert->Type::Failure->set();
+            $Alert->message = 'No resources to boot!';
+            $Alert->render();
+            return false;
+         }
+
+         // TODO get resources dirs dynamically
+         $resource_dirs = [
+            'projects/',
+            'public/',
+            'scripts/',
+            'tests/',
+            'workdata/',
+         ];
+
+         // ?
+         foreach ($resource_dirs as $dir) {
+            if (is_dir(BOOTGLY_WORKING_DIR . $dir) === true) {
+               return false;
+            }
+         }
+
+         // @
+         foreach ($resource_dirs as $dir) {
+            $resource_dir_copied = copy_recursively(BOOTGLY_ROOT_DIR . $dir, BOOTGLY_WORKING_DIR . $dir);
+            if ($resource_dir_copied === false) {
+               $Alert->Type::Failure->set();
+               $Alert->message = 'Failed to copy resource dir: ' . $dir;
+               $Alert->render();
+            }
+            else {
+               $Alert->Type::Success->set();
+               $Alert->message = 'Resource dir copied: ' . $dir;
+               $Alert->render();
+            }
+         }
+      }
+
+      $Output->write(PHP_EOL);
+
+      return true;
+   }
+}
