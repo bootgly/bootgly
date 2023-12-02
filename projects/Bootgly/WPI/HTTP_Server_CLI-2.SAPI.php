2b9ab99cfbce3d1ca35736330afa96211ff98c83
Rodrigo Vieira <rodrigo@slayer.tech>
2023-12-01 21:40:16 -0300

    🚧 Prepare support to multiples SAPI scripts

---
+++ projects/Bootgly/WPI/HTTP_Server_CLI-2.SAPI.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace projects\Bootgly\WPI;
+
+
+use Bootgly\WPI\Modules\HTTP\Server\Router;
+use Bootgly\WPI\Nodes\HTTP\Server\CLI\Request;
+use Bootgly\WPI\Nodes\HTTP\Server\CLI\Response;
+
+
+return static function
+(Request $Request, Response $Response, Router $Router)
+{
+   // ? Router
+   yield $Router->route('/', function ($Request, $Response) {
+      return $Response(body: 'Hello World!');
+   }, GET);
+   yield $Router->route('/new', function () {
+      return new Response(body: 'Testing Bootgly HTTP Router!');
+   }, GET);
+   yield $Router->route('/user/:*', function () use ($Router) {
+      yield $Router->route('', function ($Request, $Response) {
+         return $Response(body: 'Your profile!');
+      });
+
+      yield $Router->route('maria', function ($Request, $Response) {
+         return $Response(body: 'Maria\'s user profile!');
+      });
+
+      yield $Router->route('bob', function ($Request, $Response) {
+         return $Response(body: 'Bob\'s user profile!');
+      });
+   }, GET);
+
+   return yield $Response(code: 404, body: '404 Not Found!');
+};
