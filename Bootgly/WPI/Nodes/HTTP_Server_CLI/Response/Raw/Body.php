b6d48d739a6bb61a78502b111641e6e74800e27d
Rodrigo Vieira <rodrigo@slayer.tech>
2024-04-28 23:46:57 -0300

    🐛 Do not cache Body len

--- Bootgly/WPI/Nodes/HTTP_Server_CLI/Response/Raw/Body.php
+++ Bootgly/WPI/Nodes/HTTP_Server_CLI/Response/Raw/Body.php
@@ -17,46 +17,23 @@ class Body
    // ...
 
    // * Data
-   public string $raw;
+   public string $raw = '';
 
    // * Metadata
-   public int $length;
-   // TODO move
-   public static array $mimes; // @ 'html' => 'text/html'
+   private int $length;
+   // Encoded
+   private string $chunked;
 
-
-   public function __construct ()
-   {
-      // * Config
-      // ...
-
-      // * Data
-      $this->raw = '';
-
-      // * Metadata
-      $this->length = 0;
-      // mimes
-      /*
-      $items = file(__DIR__ . '/resources/mime.types', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-
-      foreach ($items as $content) {
-         if ( preg_match("/\s*(\S+)\s+(\S.+)/", $content, $match) ) {
-            $type       = $match[1];
-            $extensions = explode(' ', substr($match[2], 0, -1));
-
-            foreach ($extensions as $extension) {
-               static::$mimes[$extension] = $type;
-            }
-         }
-      }
-      */
-   }
 
    public function __get (string $name) : string
    {
       switch ($name) {
+         // * Metadata
+         case 'length':
+            return \strlen($this->raw);
+         // Encoded
          case 'chunked':
-            return \dechex($this->length) . "\r\n$this->raw\r\n";
+            return \dechex(\strlen($this->raw)) . "\r\n$this->raw\r\n";
 
          default:
             return '';
