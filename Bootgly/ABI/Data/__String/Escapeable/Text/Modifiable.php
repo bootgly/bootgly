34b14fe75cbf78923a626897308a33e05122c602
Rodrigo Vieira <rodrigo@slayer.tech>
2024-05-01 22:25:56 -0300

    📝 Added PHPDoc to Escapeable\Text\Modifiable +\       - 🆕 _TEXT_ERASE_IN_DISPLAY_3

--- Bootgly/ABI/Data/__String/Escapeable/Text/Modifiable.php
+++ Bootgly/ABI/Data/__String/Escapeable/Text/Modifiable.php
@@ -20,36 +20,69 @@ trait Modifiable
 
 
    // * Metadata
-   // [<n>@] (Insert Character)
-   // "Insert <n> spaces at the current cursor position, shifting all existing text to the right.
-   // Text exiting the screen to the right is removed."
+   /** [\<n\>@] (Insert Character)
+    * Insert <n> spaces at the current cursor position, shifting all existing text to the right.
+    * Text exiting the screen to the right is removed.
+    */
    public const _TEXT_INSERT_CHARACTER = '@';
-   // [<n>P] (Delete Character)
-   // "Delete <n> characters at the current cursor position,
-   // shifting in space characters from the right edge of the screen."
+
+   /** [\<n\>P] (Delete Character)
+    * Delete <n> characters at the current cursor position, shifting in space characters from the right edge of the screen.
+    */
    public const _TEXT_DELETE_CHARACTER = 'P';
-   // [<n>X]  (Erase Character)
-   // "Erase <n> characters from the current cursor position by overwriting them with a space character."
+
+   /** [\<n\>X]  (Erase Character)
+    * Erase <n> characters from the current cursor position by overwriting them with a space character.
+    */
    public const _TEXT_ERASE_CHARACTER = 'X';
 
-   // [<n>L]  (Insert Line) "Inserts <n> lines into the buffer at the cursor position.
-   // The line the cursor is on, and lines below it, will be shifted downwards."
-   public const _TEXT_INSERT_LINE = 'L';
-   // [<n>M]  (Delete Line) "Deletes <n> lines from the buffer, starting with the row the cursor is on."
-   public const _TEXT_DELETE_LINE = 'M';
 
-   // [<n>K] (Erase in Line) "Replace all text on the line with the cursor specified by <n> with space characters"
+   /** [\<n\>L]  (Insert Line)
+    * Inserts <n> lines into the buffer at the cursor position.
+    * The line the cursor is on, and lines below it, will be shifted downwards.
+    */
+   public const _TEXT_INSERT_LINE = 'L';
+   /** [\<n\>M]  (Delete Line)
+    * Deletes <n> lines from the buffer, starting with the row the cursor is on.
+    */
+   public const _TEXT_DELETE_LINE = 'M';
+   /** [\<n\>K] (Erase in Line)
+    * Replace all text on the line with the cursor specified by \<n\> with space characters
+    */
    public const _TEXT_ERASE_IN_LINE = 'K';
 
-   // [<n>J] (Erase in Display) "Replace all text in the current viewport/screen specified by <n> with space characters"
+   /** [\<n\>J] (Erase in Display)
+    * Replace all text in the current viewport/screen specified by \<n\> with space characters
+    */
    public const _TEXT_ERASE_IN_DISPLAY = 'J';
 
+   /**
+    * Clear the lines below the current cursor position
+    */
+   public const _TEXT_ERASE_IN_DISPLAY_0 = '0J';
+   /**
+    * Clear the lines above the current cursor position
+    */
+   public const _TEXT_ERASE_IN_DISPLAY_1 = '1J';
+   /**
+    * Clear the entire screen / display
+    */
+   public const _TEXT_ERASE_IN_DISPLAY_2 = '2J';
+   /**
+    * Clear the entire screen / display and delete all lines saved in the scrollback buffer
+    */
+   public const _TEXT_ERASE_IN_DISPLAY_3 = '3J';
 
-   public const _TEXT_ERASE_IN_DISPLAY_0 = '0J'; // Clear the lines below the current cursor position
-   public const _TEXT_ERASE_IN_DISPLAY_1 = '1J'; // Clear the lines above the current cursor position
-   public const _TEXT_ERASE_IN_DISPLAY_2 = '2J'; // Clear the entire screen / display
-
-   public const _TEXT_ERASE_IN_LINE_0 = '0K'; // To Right from cursor
-   public const _TEXT_ERASE_IN_LINE_1 = '1K'; // To Left from cursor
-   public const _TEXT_ERASE_IN_LINE_2 = '2K'; // To Left and Right from cursor
+   /**
+    * Clear the characters from cursor to the right
+    */
+   public const _TEXT_ERASE_IN_LINE_0 = '0K';
+   /**
+    * Clear the characters from cursor to the left
+    */
+   public const _TEXT_ERASE_IN_LINE_1 = '1K';
+   /**
+    * Clear the characteres from cursor to the left and right (entire line)
+    */
+   public const _TEXT_ERASE_IN_LINE_2 = '2K';
 }
